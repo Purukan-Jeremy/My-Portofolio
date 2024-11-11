3850ef751ee9c425c1edeef283fd7305dfe3fe44
Purukan-Jeremy <s22210214@student.unklab.ac.id>
2024-11-11 17:48:20 +0800

    ada tambah skills sambungan dri repo seblah

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './assets/style.css';
+import Home from './components/Home'
+import Hero from './components/Hero'
+import Skills from './components/Skills';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+      />
+      <link rel="stylesheet" href="../src/assets/style.css" />
+      <title>Jeremy Website</title>
+      <header>
+        <a href="#" className="logo">Purukan Jeremy</a>
+        <nav>
+          <a href="#" className="active">Home</a>
+        </nav>
       </header>
+      <Home/>
+      <Skills/>
+      <Hero/>
+      
     </div>
   );
-}
+};
 
 export default App;
